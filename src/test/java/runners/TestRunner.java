c021d881f196cfca441b90f575e7144adca659de
SWRV Tech <admin@swrvtech.com>
2024-07-17 08:41:31 -0400

    UDPT

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features/",
         glue = "APISteps",
-        dryRun = true,
+        dryRun = false,
 		tags = "@jsonpayload",
         monochrome = true,
 
